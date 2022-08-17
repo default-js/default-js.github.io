de2e3a79a95864b558c1a393db6756b579bb7c77
Frank Schüler <SchuelerFrank@gmx.de>
2022-08-17 12:19:15 +0200

    add new repo and reduse custom page code

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,28 @@
+/*
+ * styles from libs
+ */
+
+import "@default-js/defaultjs-html-dropdown/src/css/dropdown.css";
+/*
+ * custom styles 
+ */
 import "../css/fontawesome.css";
 import "../css/base.css";
+import "../css/dropdown.css";
 import "../css/navigation.css";
 import "../css/highlight.css";
-import "../css/dropdown.css";
 import "../css/layout.css";
 
+/*
+ * libs
+ */
 import "@default-js/defaultjs-extdom";
 import "@default-js/defaultjs-app-toolbox";
-import "./components/GithubMarkdownRenderServiceElement";
-import "./components/DropdownElement";
 import "@default-js/defaultjs-html-renderer";
+import "@default-js/defaultjs-html-dropdown";
 
+/*
+ * custom scripts
+ */
+
+import "./components/GithubMarkdownRenderServiceElement";
